a27e62ba561af8d4f25897264ceb6546f5f6454c
alpinagyok <aleks.kozaev@gmail.com>
2020-06-17 16:28:43 +0300

    redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom"; // browserrouter allows to press 'back' and get prev route etc.?
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
-
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          {/* no container around landing because we want it on the whole screen */}
-          {/* without exact it will also show '/login' for example */}
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            {/* no container around landing because we want it on the whole screen */}
+            {/* without exact it will also show '/login' for example */}
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers"; // we called it index, so no need to write it
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    // compose is just to add devtools
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
