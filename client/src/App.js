b0143ec7951e983cc045bf7dc5319ba4fe26c2f9
alpinagyok <aleks.kozaev@gmail.com>
2020-06-17 15:56:24 +0300

    router + login and register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"; // browserrouter allows to press 'back' and get prev route etc.?
+
 import "./App.css";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          {/* no container around landing because we want it on the whole screen */}
+          {/* without exact it will also show '/login' for example */}
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- validation/register.js
+++ validation/register.js
@@ -18,12 +18,13 @@ module.exports = validateRegisterInput = (data) => {
     errors.name = "Name field is required";
   }
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  }
-
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
+  }
+
+  // isEmail will also check for emptiness
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
   }
 
   if (Validator.isEmpty(data.password)) {
