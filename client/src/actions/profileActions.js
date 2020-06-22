8d943089bb14219c46d09db9a3382cc028e8211a
alpinagyok <aleks.kozaev@gmail.com>
2020-06-22 21:03:30 +0300

    Profile Reducer + Get Current

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE,
+} from "./types";
+
+// when do we need dispatch??
+
+// Get current profile
+export const getCurrentProfile = () => (dispatch) => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then((res) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    // we don't want an error here, because a user can have no profile
+    .catch((err) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {},
+      })
+    );
+};
+
+// Profile Loading
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING,
+  };
+};
+
+// Clear Profile
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE,
+  };
+};
