62c165db91be42ddd53fcc8447a116d684701491
Aleks Kozajev <aleks.kozaev@gmail.com>
2020-07-06 12:59:09 +0300

    Profile by handle Sub Components

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -31,6 +31,25 @@ export const getCurrentProfile = () => (dispatch) => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = (handle) => (dispatch) => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then((res) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    .catch(() =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null,
+      })
+    );
+};
+
 // Create Profile      // history is for redirecting
 export const createProfile = (profileData, history) => (dispatch) => {
   axios
